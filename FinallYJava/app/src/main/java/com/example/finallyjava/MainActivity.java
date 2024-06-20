659a21eac7d3ad5a65d59487f4c2f9195087a175
rambo-san <abhisuresh2020@gmail.com>
2024-06-20 15:40:53 +0530

    First

--- FinallYJava/app/src/main/java/com/example/finallyjava/MainActivity.java
+++ FinallYJava/app/src/main/java/com/example/finallyjava/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.finallyjava;
 import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +19,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView)findViewById(R.id.baseText);
-        tv.setText("Hello World");
+        TextView tv1 = (TextView)findViewById(R.id.t1);
+        TextView tv2 = (TextView)findViewById(R.id.t2);
+        TextView tv3 = (TextView)findViewById(R.id.t3);
+        TextView tv4 = (TextView)findViewById(R.id.t4);
+        TextView tv5 = (TextView)findViewById(R.id.t5);
+        TextView tv6 = (TextView)findViewById(R.id.t6);
+        TextView tv7 = (TextView)findViewById(R.id.t7);
+        TextView tv8 = (TextView)findViewById(R.id.t8);
+        TextView tv9 = (TextView)findViewById(R.id.t9);
+        TextView tv10 = (TextView)findViewById(R.id.t10);
+
+        Calendar c= Calendar.getInstance();
+        SimpleDateFormat s=new SimpleDateFormat("dd.mm.yyyy");
+        String time= s.format(c.getTime());
+        tv1.setText(time.toString());
+        s=new SimpleDateFormat("dd.mm.yyyy HH:mm:ss");
+        time= s.format(c.getTime());
+        tv2.setText(time.toString());
+        s=new SimpleDateFormat("EEEE");
+        time= s.format(c.getTime());
+        tv3.setText(time.toString());
+        s=new SimpleDateFormat("E");
+        time= s.format(c.getTime());
+        tv4.setText(time.toString());
+        tv5.setText(time.toString());
+        tv6.setText(time.toString());
+        tv7.setText(time.toString());
+        tv8.setText(time.toString());
+        tv9.setText(time.toString());
+        tv10.setText(time.toString());
+        Toast.makeText(MainActivity.this, "Hello Bro", Toast.LENGTH_LONG).show();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
