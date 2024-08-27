a5ff5a307eb9c1851d0016408547e5d1cf3895a9
rambo-san <abhisuresh2020@gmail.com>
2024-08-27 10:16:48 +0530

    ddb

--- Db_first/app/src/main/java/com/example/db_first/DBHandler.java
+++ Db_first/app/src/main/java/com/example/db_first/DBHandler.java
@@ -1,4 +1,27 @@
 package com.example.db_first;
 
-public class DBHandler {
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+public class DBHandler extends SQLiteOpenHelper{
+    private static final String DB_NAME="student_db", TABLE_NAME="student_details", ID="id",NAME="name",COURSE="course", SEMESTER="semester";
+    private static final int DB_VERSION=1;
+    //constructor
+    public DBHandler(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    String query="CREATE TABLE "+TABLE_NAME+"("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+NAME+" TEXT, "+COURSE+" TEXT, "+SEMESTER+" INTEGER)";
+    db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+    onCreate(db);
+    }
 }
