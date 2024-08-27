6e69422a466ea0a2670f48ca10def37bd424e261
rambo-san <abhisuresh2020@gmail.com>
2024-08-27 10:16:48 +0530

    ddb

--- Services/app/src/main/java/com/example/services/MainActivity.java
+++ Services/app/src/main/java/com/example/services/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.db_first;
+package com.example.services;
 
 import android.os.Bundle;
 
@@ -7,39 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import  android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-private EditText e1,e2,e3;
-private DBHandler dbHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        e1=findViewById(R.id.name);
-        e2=findViewById(R.id.course);
-        e3=findViewById(R.id.semester);
-        dbHandler=new DBHandler(MainActivity.this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-    }
-    public void addStudent(View v) {
-//get values from editText
-        String name=e1.getText().toString();
-        String course=e2.getText().toString();
-        int semester=Integer.parseInt(e3.getText().toString());
-        dbHandler.addNewStudent(name,course,semester);
-        Toast.makeText(MainActivity.this,"Student Added successfully",Toast.LENGTH_LONG).show();
-        e1.setText("");
-        e2.setText("");
-        e3.setText("");
-
     }
 }
